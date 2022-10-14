980cce230908976ba2573759fe011fd1b5f635ec
sebastianduchene <sebastian.duchene6@gmail.com>
2022-10-14 14:06:23 +1100

    update steps right before sequence simulation

--- sandbox/workflow_testing.py
+++ sandbox/workflow_testing.py
@@ -4,16 +4,20 @@ import os, sys, re
 
 replicates = [i for i in range(5)] # number of replicates to run
 
-template = 'MPXV_speciesjump_common.xml' # Template xml
-prefix = 'set1_MPXV_common_r' # the prefix we want to use
+template = 'MPXV_speciesjump_rare.xml' # Template xml
+prefix = 'set1_MPXV_rare_r' # the prefix we want to use
 
 
-for i in replicates:
+for i in replicates[:3]:
     replicate_xml = open(template, 'r').read()
     output_name = prefix+str(i)
     replicate_xml = re.sub('OUTPUT_FILE_NAME', output_name, replicate_xml)
     open(output_name + '.xml', 'w').writelines(replicate_xml)
     os.system('~/phyloApps/beast2.6.3/bin/beast ' + output_name+'.xml')
+    os.system('./treePostProcessing.py -t ' + output_name + '.nexus.tree -lh 1')
+# Here execute code to simulate the sequence alignment   
+
+
 
 #for i in $replicaes; do
 #    sed -i.bak 's/OUTPUT_FILE_NAME/$i/g' MPXV_speciesjump_common.xml
